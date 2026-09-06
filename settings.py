9d632cb2cab63fe6eba1e564fa98d9423e633599
Oreshek <KedrowOreshek@yandex.ru>
2026-09-06 11:15:44 +0300

    исправленная версия

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()  # загружаем переменные из .env
 
+
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
